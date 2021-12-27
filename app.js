6acd4c2596c2ed2aa66658e0c192da2a2ef3de4f
jh-jin <devjin0122@gmail.com>
2021-12-27 15:17:43 +0900

    '3.1HTMLinJS'

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const title = document.getElementById("title");
 
 //console.dir(title);
-title.innerText = "Got you!"
+//title.innerText  = "Got you!"
 
 console.log(title.id);
 console.log(title.className);
