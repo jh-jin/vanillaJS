f8ab7338e0f1a1cc6209ddf5a85a7b331f9863ca
jh-jin <devjin0122@gmail.com>
2021-12-27 16:38:33 +0900

    '3.2_Searching_for_Elements'

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-const title = document.getElementById("title");
-
-//console.dir(title);
-//title.innerText  = "Got you!"
-
-console.log(title.id);
-console.log(title.className);
+//3.2 Searching for Elements: 원소 가져오기
+let hellos = [];
+hellos[0] = document.getElementsByClassName("hello0");
+hellos[1] = document.getElementsByTagName("h1");
+hellos[2] = document.getElementById("hey")
+//*중요* CSS selector로 가져오기
+hellos[3] = document.querySelector(".hello1 h1"); //첫 하나 가져오기
+hellos[4] = document.querySelectorAll(".hello1 h1") //해당 모두 가져오기
+hellos[5] = document.querySelector(".hello1 h1:last-child")
+console.log(hellos);
