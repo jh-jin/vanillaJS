2188e16074794212724badfb607527c1b4a5c8c3
jh-jin <devjin0122@gmail.com>
2021-12-27 23:10:09 +0900

    '4.4'

---
+++ 4.4_Getting_Username.js
@@ -0,0 +1,16 @@
+
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden";
+
+function onLoginSubmit(event){ //첫 인자는 event로 받는 게 관행
+    event.preventDefault(); //브라우저의 기본 동작을 막음. 여기선 제출 시 새로고침 막기
+
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    greeting.innerHTML = "Hello "+username;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
