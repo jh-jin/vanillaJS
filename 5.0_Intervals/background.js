de606d4d361ec8a1d165460abc919f61b9ddb227
jh-jin <devjin0122@gmail.com>
2021-12-30 10:07:17 +0900

    '5.2_background'

--- 5.0_Intervals/background.js
+++ 5.0_Intervals/background.js
@@ -1,9 +1,10 @@
-const images = ["0.jpg", "1.jpg", "2.jpeg"];
+//document.createElement
 
+const images = ["0.jpg", "1.jpg", "2.jpeg"];
 const chosenImage = images[Math.floor(Math.random()*images.length)];
 
-const bgImage = document.createElement("img");
-
+const bgImage = document.createElement("img"); // !
 bgImage.src = `img/${chosenImage}`;
 
 document.body.appendChild(bgImage);
+//document.body.prepend(bgImage); //맨 앞
