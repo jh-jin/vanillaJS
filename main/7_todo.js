36c58f79a85a79391da96137b494205164f238c3
jh-jin <devjin0122@gmail.com>
2021-12-30 15:45:44 +0900

    'stringify_parse_forEach'

--- main/7_todo.js
+++ main/7_todo.js
@@ -1,9 +1,13 @@
 //localStorage에 array를 저장할 수 없다 only String!
 //JSON.stringify()
+//JSON.parse()
+//.forEach()
+
 const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "todos";
 const toDos = [];
 
 function deleteToDo(event){
@@ -11,9 +15,8 @@ function deleteToDo(event){
     li.remove();
 }
 
-function saveToDos(newToDo){
-    localStorage.setItem("todos", JSON.stringify(toDos));
-
+function saveToDos(){
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
 function paintToDo(newToDo){
@@ -28,7 +31,6 @@ function paintToDo(newToDo){
     button.addEventListener("click", deleteToDo);
 }
 
-
 function handleToDoSubmit(event){
     event.preventDefault(); //새로고침 방지
     const newToDo = toDoInput.value;
@@ -39,3 +41,16 @@ function handleToDoSubmit(event){
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+function sayHello(item){
+    console.log("this is the turn of", item);
+}
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+console.log("S",savedToDos);
+
+if(savedToDos !== null){
+    const parsedToDos = JSON.parse(savedToDos);
+    console.log("P",parsedToDos);
+    parsedToDos.forEach(sayHello);
+}
